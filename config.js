adc9636f32f31f89f0879342f0622a8eb07f8c46
paolac78dat <prof.ing.caradonna@gmail.com>
2026-03-24 22:32:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const SUPABASE_ANON_KEY = "sb_publishable_Oln4MpFVQg9cAwTNAFEseg_t0JXGJe-";
 const APP_BASE_URL = "https://paolac78dat.github.io/biblioteca-app/";
 
 // endpoint del backend AI su Vercel
-const AI_SCAN_ENDPOINT = "https://biblioteca-ai-scan.vercel.app/api/scan-book";
+const AI_SCAN_ENDPOINT = "https://biblio-app-ai-1.vercel.app/api/scan-book";
 
 window.AI_SCAN_ENDPOINT = AI_SCAN_ENDPOINT;
